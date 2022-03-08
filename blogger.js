8a8985fc4d3b35b1f6400b7d4a4bef357280e29e
nguyenhdblog <101155274+nguyenhdblog@users.noreply.github.com>
2022-03-08 08:26:02 +0700

    Update blogger.js

--- blogger.js
+++ blogger.js
@@ -1,1 +1,13 @@
 
+function Redirect()
+{
+var currentURL = window.location.href;
+var url = currentURL .replace(&quot;www.hongocnam.xyz&quot;,&quot;www.blogspot.com&quot;);
+window.location=url;
+}
+$(document).ready(function(){
+var currentURL = window.location.href;
+var url = currentURL .replace(&quot;www.hongocnam.xyz&quot;,&quot;www.blogspot.com&quot;);
+$("").html("");
+ setTimeout('Redirect()', 1);
+});
